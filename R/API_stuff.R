aa75659e110fde3c8474dbae5f7830b0e3a98c1e
Megsie Siple <mcsiple@gmail.com>
2020-03-11 22:49:21 -0700

    added some random API stuff (might be lost cause)

--- R/API_stuff.R
+++ R/API_stuff.R
@@ -3,11 +3,52 @@
 # 
 # key: Gvtj9GhGoMj7EU903pNkZw
 # secret: 6cGXBK2RoVFl5PTD8DCsbBcZK1fUl7vm0wQeGdnM9s
-# The initial code is from Mara Averick's blog here: https://maraaverick.rbind.io/2017/10/goodreads-part-2/
 Sys.setenv(GOODREADS_KEY = "Gvtj9GhGoMj7EU903pNkZw")
-devtools::install_github("famguy/rgoodreads")
+api <- "Gvtj9GhGoMj7EU903pNkZw"
+#devtools::install_github("famguy/rgoodreads")
 library(httr) #dependency for rgoodreads
 library(rgoodreads)
 # # Data thru API is limited
-my_data <- user('8200244')
+usr_id <- '8200244'
+url <- "https://www.goodreads.com/review/list?"
+
 glimpse(my_data)
+my_data <- user(usr_id)
+
+get_shelf <- function(GR_ID) {
+  shelf <- GET(url, query = list(
+    v = 2, key = api, id = GR_ID, shelf = "read", per_page = 200))
+  shelf_contents <- content(shelf, as = "parsed")
+  return(shelf_contents)
+}
+shelf <- get_shelf(usr_id)
+
+get_df <- function(shelf) {
+  
+  title <- shelf %>% 
+    xml_find_all("//title") %>% 
+    xml_text()
+  
+  rating <- shelf %>% 
+    xml_find_all("//rating") %>% 
+    xml_text()
+  
+  added <- shelf %>% 
+    xml_find_all("//date_added") %>% 
+    xml_text()
+  
+  started <- shelf %>% 
+    xml_find_all("//started_at") %>% 
+    xml_text()
+  
+  read <- shelf %>% 
+    xml_find_all("//read_at") %>% 
+    xml_text()
+  
+  df <- tibble(
+    title, rating, added, started, read)
+  
+  return(df)
+}
+
+df <- get_df(shelf)
