1e507acc921b8738502a5ca294d6f90b1f0e37db
Megsie Siple <mcsiple@gmail.com>
2020-03-18 13:56:59 -0700

    Add color palette, basic diagnostics, raw csv input

--- NovelGazingApp/app.R
+++ NovelGazingApp/app.R
@@ -3,7 +3,21 @@ library(shiny)
 library(shinythemes)
 library(tidyverse)
 
-# Define UI for application that draws a histogram
+source(here('R','cleanup_csv.R'))
+source(here('R','basic_diagnostics.R'))
+
+
+# Aesthetics --------------------------------------------------------------
+bookpal <- c('#ff7506', #dark orange
+             '#00eaff', #(cyan)
+             '#ffdf06', #(dark yellow)
+             '#ffee7d', #(light yellow)
+             '#ff2673', #(magenta )
+             '#7df4ff') #(light cyan )
+
+
+
+# ui ----------------------------------------------------------------------
 ui <- navbarPage('Novel-Gazing',
                  theme = shinytheme("yeti"),
                  tabPanel("1. Your books",
@@ -20,9 +34,10 @@ ui <- navbarPage('Novel-Gazing',
       ),
       mainPanel(
          h3('Your reading at a glance'),
-         h4('Check your data'),
+         h4('A glimpse of your data'),
          tableOutput('rawdata'),
-         
+         h4('Reading history'),
+         plotOutput('basicstuff')
          
       ))),
    tabPanel("2. Reading history"),
@@ -30,24 +45,41 @@ ui <- navbarPage('Novel-Gazing',
    )
 
 
-# Define server logic required to draw a histogram
+
+# Server ------------------------------------------------------------------
 server <- function(input, output) {
    # Image
    output$preImage <- renderImage({
-      filename <- normalizePath(here::here('NovelGazingApp','images','test.jpg'))
+      filename <- normalizePath(here::here('NovelGazingApp','images','book_logo.png'))
       # Return a list containing the filename and alt text
       list(src = filename,
-           alt = 'test')
+           alt = 'test',
+           width = 240,
+           height = 240)
    }, deleteFile = FALSE)
    
    # Raw data
+   rawdatafile <- reactive({
+      inFile <- input$grexport
+      if (is.null(inFile)) return(NULL)
+      x <- read.csv(inFile$datapath, header = TRUE)
+      x
+   })
+   
    output$rawdata <- renderTable({
-     inFile <- input$grexport
-     if (is.null(inFile)) return(NULL)
-     x <- read.csv(inFile$datapath, header = TRUE)
+   if (is.null(rawdatafile())) {NULL}else{
+     x <- rawdatafile()
      show <- x %>% 
               select(Title, Author, ISBN, My.Rating,Number.of.Pages)
      head(show,n = 3)
+   }
+   })
+   
+   # Basic diagnostic plots
+   output$basicstuff <- renderPlot({
+      if(is.null(rawdatafile())){NULL}else{
+      plotdat <- cleanup_csv(rawdatafile())
+      basic_diagnostics(plotdat,pal = bookpal)}
    })
    
 }
