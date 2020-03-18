138e8a623ee9e49d1c13702ee8f73e0c8de4fae7
Megsie Siple <mcsiple@gmail.com>
2020-03-18 13:56:33 -0700

    Put together multipanel plot

--- R/basic_diagnostics.R
+++ R/basic_diagnostics.R
@@ -10,23 +10,35 @@ library(snakecase)
 library(patchwork)
 
 # Load your own user data separately
-testfile <- here::here('data','goodreads_library_export.csv')
+#testfile <- here::here('data','goodreads_library_export.csv')
+#cleaned_csv1 <- cleanup_csv(read.csv(testfile,header = TRUE))
+  
 
-read_books <- cleanup_csv(csvfile = testfile)
-
-basic_diagnostics <- function(read_books){
-  bbreaks <- seq(min(read_added$year,na.rm=T),
-                 max(read_added$year,na.rm=T),by = 2)
+basic_diagnostics <- function(cleaned_csv = cleaned_csv1,
+                              pal = bookpal){
+  read_books <- cleaned_csv
+  bbreaks <- seq(min(read_books$year_added,na.rm=T),
+                 max(read_books$year_added,na.rm=T),by = 2)
   
   BPY <- read_books %>%
     ggplot(aes(year_read)) +
-    geom_bar(stat = "count") +
+    geom_bar(stat = "count",fill=pal[6]) +
     xlab('Year') +
-    ylab('Count') +
-    labs(title = "Books read per year", 
-         caption = "source: Goodreads") +
+    ylab('Books') +
+    # labs(#title = "Books per year", 
+    #      caption = "source: Goodreads") +
     scale_x_continuous(breaks=bbreaks,
-                       labels=bbreaks)
+                       labels=bbreaks) +
+    ggsidekick::theme_sleek(base_size = 12)
+  
+  PPY <- read_books %>%
+    ggplot(aes(x = year_read, y = sum(number_of_pages,na.rm=T))) +
+    geom_col(fill = pal[6]) +
+    xlab('Year') +
+    ylab('Pages') +
+    scale_x_continuous(breaks=bbreaks,
+                       labels=bbreaks) +
+    ggsidekick::theme_sleek(base_size = 12)
   
   criticplot <- read_books %>% 
     group_by(year_read) %>% 
@@ -38,40 +50,43 @@ basic_diagnostics <- function(read_books){
                      xend=year_read),
                  col="darkgrey",lwd=0.5) +
     geom_point(aes(x=year_read,y=allusers_mean_rating),colour="grey",size=3) + 
-    geom_point(aes(x=year_read,y=my_mean_rating),colour="#d95f0e",size=3) +
+    geom_point(aes(x=year_read,y=my_mean_rating),colour=pal[1],size=3) +
     ylab("Average rating") +
     xlab("Year") +
     scale_x_continuous(breaks=bbreaks,
                        labels=bbreaks) +
-    ggtitle("Have you become a more critical reader?") +
-    ggsidekick::theme_sleek(base_size=14)
+    labs(title = "Have you become a more critical reader?",
+         caption = "source: Goodreads") +
+    ggsidekick::theme_sleek(base_size=12) +
+    theme(legend.position = 'bottom')
   
-   x <- read_books %>%
+  x <- read_books %>%
     group_by(year_added) %>%
     summarize(n1 = length(title)) %>%
-              mutate(cn = cumsum(n1),
-                     AR = 'Added') %>%
-     rename(year = year_added)
-    y <- read_books %>%
-     group_by(year_read) %>%
-     summarize(n2 = length(title)) %>%
-      mutate(cn = cumsum(n2),
-             AR = 'Finished') %>%
-      rename(year = year_read)
+    mutate(cn = cumsum(n1),
+           AR = 'Added') %>%
+    rename(year = year_added)
+  y <- read_books %>%
+    group_by(year_read) %>%
+    summarize(n2 = length(title)) %>%
+    mutate(cn = cumsum(n2),
+           AR = 'Finished') %>%
+    rename(year = year_read)
   
-    read_added <- bind_rows(x,y)
-    
-    cumubooks <- 
-      read_added %>%
-      ggplot(aes(x = year,y = cn,fill = AR)) + 
-      geom_area() +
-      scale_x_continuous(breaks = bbreaks) +
-      ggsidekick::theme_sleek() +
-      scale_fill_manual('',values = ghibli::ghibli_palette('YesterdayLight')[5:6]) +
-      labs(title = 'Cumulative books finished or added') +
-      xlab('Year') +
-      ylab('Number of books')
+  read_added <- bind_rows(x,y)
   
-    
-    
+  cumubooks <- 
+    read_added %>%
+    ggplot(aes(x = year,y = cn,fill = AR)) + 
+    geom_area(size=0.5,colour='white') +
+    scale_x_continuous(breaks = bbreaks) +
+    ggsidekick::theme_sleek(base_size = 12) +
+    scale_fill_manual('',values = pal[3:4]) +
+    xlab('Year') +
+    ylab('Cumulative books finished or added') +
+    theme(legend.position = 'bottom')
+  
+  (BPY | PPY) / (cumubooks | criticplot)
 }
+
+#basic_diagnostics(cleaned_csv = cleaned_csv1,pal = bookpal)
