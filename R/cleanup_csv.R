f68a1e1dd9bc3b81837bfad30b3efac999bba072
Megsie Siple <mcsiple@gmail.com>
2020-03-24 15:24:06 -0700

    add ISBN to outputs

--- R/cleanup_csv.R
+++ R/cleanup_csv.R
@@ -1,10 +1,11 @@
-library(lubridate)
-library(here)
-library(tidyverse)
-library(snakecase)
+# library(lubridate)
+# library(here)
+# library(tidyverse)
+# library(snakecase)
 
 #testfile <- here::here('data','goodreads_library_export.csv')
 #my_books <- read.csv(testfile,header = TRUE)
+#cleaned_books <- cleanup_csv(my_books=my_books)
 
 cleanup_csv <- function(my_books){
   # my_books is a dataframe from read.csv()
@@ -12,11 +13,14 @@ cleanup_csv <- function(my_books){
   new_colnames <- to_any_case(orig_colnames, case = "snake")
   colnames(my_books) <- new_colnames  # assign new colnames
   colnames(my_books)  # check to make sure it worked
-  keeper_cols <- c("book_id", "title", "author", "my_rating", "number_of_pages", 
+  keeper_cols <- c("book_id", "title", "author", "isbn", "my_rating", "number_of_pages", 
                    "original_publication_year", "date_read", "date_added", "bookshelves",
                    "exclusive_shelf","my_rating","average_rating")
+  
   read_books <- my_books %>%
     select(one_of(keeper_cols)) %>%
+    mutate(isbn = str_remove_all(isbn, '[^[:alnum:]]')) %>%
+    mutate(isbn = as.character(isbn)) %>%
     mutate(date_read = ymd(date_read),
            year_read = year(date_read),
            date_added = ymd(date_added),
