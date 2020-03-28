42941c397505bb9c2f201e828a692cbf6848c9ed
Megsie Siple <mcsiple@gmail.com>
2020-03-27 18:15:23 -0700

    Remove scraped data and clean up subject text

--- R/genre_diversity.R
+++ R/genre_diversity.R
@@ -1,13 +1,27 @@
 # genre diversity over time
 # 
-get_rainbow_plot <- function(scraped_data,pal,whichplot){
+get_rainbow_plot <- function(comm_genre,pal,whichplot){
+  #comm_genre is the result of get_cmatrix_and_genres(fulldata = full_data)
   #whichplot is 'yearly' or 'monthly'
-  g <- get_cmatrix_and_genres(scraped_data = scraped_data)$genres
+  
+  if(whichplot == 'yearly'){
+  #cleanup genres
+  g <- comm_genre$genres
+  g <- g %>%
+    filter(!is.na(book_genres)) %>%
+    filter(!is.na(year_read)) %>%
+    filter(textcat::textcat(book_genres)=='english') %>%
+    mutate(book_genres = sub("\\ /.*", "", book_genres)) %>%
+    mutate(book_genres = sub("\\ --.*", "", book_genres)) %>%
+    mutate(book_genres = stringr::str_to_sentence(book_genres)) %>%
+    mutate(book_genres = trimws(book_genres))
+  # for nested topics, take the largest one; change everything to the same case. Then change to sentence case and trim white space
   
   allgenres <- g %>%
     group_by(year_read,book_genres) %>%
-    summarize(total = length(book_genres))  %>%
+    summarize(total = length(book_genres)) %>%
     ungroup(year_read,book_genres) %>%
+    #filter(total>1) %>% 
     mutate(year_read = as.integer(year_read))
   
   top10genres <- g %>%
@@ -25,64 +39,56 @@ get_rainbow_plot <- function(scraped_data,pal,whichplot){
   mycolors <- colorRampPalette(pal)(nb.cols)
   mycolors[levels(pdat$simple_genre)=='Other'] = 'gray'
   
-  rainbowbeauty <- pdat %>% 
+  bbreaks <- seq(min(pdat$year_read,na.rm=T),max(pdat$year_read,na.rm=T),1)
+  
+  
+  rb <- pdat %>% 
     ggplot(aes(year_read,total,fill=simple_genre)) + 
     geom_col() +
+    scale_x_continuous(breaks = bbreaks) +
     scale_fill_manual('Your top genres',values = mycolors) +
     xlab('Year') +
     ylab('Count among books you finished') +
     ggsidekick::theme_sleek(base_size = 14) +
-    labs(caption = "*Note: Most books have cross-referenced,\n so the genre total will be higher than the number of books you read.") +
+    labs(caption = "*Note: Most books have cross-referenced,\n so the topic total may be higher than the number of books you read.") +
     theme(legend.position = 'none')
+  return(rb)}else{
+    
+    
+  gm <- comm_genre$genres_month
+  top10genres <- gm %>%
+    group_by(book_genres) %>%
+    summarize(bcount = length(book_genres)) %>%
+    arrange(desc(bcount)) %>%
+    top_n(bcount, n = 10)
   
-  g2 <- get_cmatrix_and_genres(scraped_data = scraped_data)$genres_month
-  
-  allgenres_month <- g2 %>%
+  allgenres_month <- gm %>%
     group_by(month_read,book_genres) %>%
     summarize(total = length(book_genres))  %>%
     ungroup(year_read,book_genres) %>%
     mutate(month_read = as.integer(month_read))
   
-  pdat2 <- allgenres_month %>%
+  pdatm <- allgenres_month %>%
     mutate(simple_genre = ifelse(book_genres %in% top10genres$book_genres,book_genres,'Other')) %>%
     group_by(month_read,simple_genre) %>%
     ungroup(simple_genre) %>%
     mutate(simple_genre = fct_relevel(simple_genre, 'Other', after = Inf))
-  nb.cols <- length(levels(pdat2$simple_genre))
+  nb.cols <- length(levels(pdatm$simple_genre))
   mycolors <- colorRampPalette(pal)(nb.cols)
-  mycolors[levels(pdat$simple_genre)=='Other'] = 'gray'
-  
-  rainbowbeauty_mo <- pdat2 %>% 
+  mycolors[levels(pdatm$simple_genre)=='Other'] = 'gray'
+ 
+  rb_mo <- pdatm %>% 
     ggplot(aes(month_read,total,fill=simple_genre)) + 
     geom_col() +
-    scale_fill_manual('Your top genres',values = mycolors) +
+    scale_fill_manual('Your top topics',values = mycolors) +
     scale_x_continuous(breaks = 1:12,labels = month.abb) +
     xlab('Month') +
     ylab('Count among books you finished \n (across all years)') +
-    ggsidekick::theme_sleek(base_size = 14)# +
-    #labs(caption = 'Total across all years') 
-  
-  if(whichplot == 'yearly'){
-    return(rainbowbeauty)
-  }else{
-    return(rainbowbeauty_mo)
+    labs(caption = 'Topic data are from the OpenLibrary API. Not all books ahve topic data available.')+
+    ggsidekick::theme_sleek(base_size = 14)
+    return(rb_mo)
   }
 }
-
-#scrape <- scrape_goodreads(npages = 1) #time-consuming
-#g <- get_cmatrix_and_genres(scraped_data = goodreads)$genres
-#bookpal <- c("#ff7506", "#00eaff", "#ffdf06", "#ffee7d", "#ff2673", "#7df4ff") 
-#get_rainbow_plot(scraped_data = goodreads,pal = bookpal,whichplot = 'yearly')  
-
-
-#library(here)
-#community <- read.csv(here('output','communitymatrix.csv'),header = T)
-#cmatrix <- as.matrix(community)
-#library(vegan)
-
-#year <- cmatrix[,1]
-#divs <- data.frame(year = year,
-#                   div = diversity(cmatrix[,-1]))
 
 get_divplot <- function(community){
   cmatrix <- as.matrix(community)
@@ -90,10 +96,12 @@ get_divplot <- function(community){
   divs <- data.frame(year = year,
                      div = diversity(cmatrix[,-1]))
   cmatrix <- as.matrix(community)
+  bbreaks <- seq(min(divs$year,na.rm=T),max(divs$year,na.rm=T),1)
   divplot <- divs %>%
     ggplot(aes(year,div)) +
     geom_point(size=3) +
     geom_line() +
+    scale_x_continuous(breaks = bbreaks) +
     ylab('Shannon diversity (H)') +
     xlab('Year') +
     labs(caption = 'Data: Goodreads') +
@@ -101,24 +109,22 @@ get_divplot <- function(community){
   return(divplot)
 }
 
-#comm <- get_cmatrix_and_genres(scraped_data = goodreads)$community
-#get_divplot(community = comm)
 
 get_rarefaction <- function(community,pal){  
   cmatrix <- as.matrix(community)
   year <- cmatrix[,1]
   sa <- specaccum(cmatrix[,-1])
   my.res <- with(sa, data.frame(sites, richness, sd)) %>%
-    mutate(year = cmatrix[,1]) %>%
+    mutate(year = year) %>%
     mutate(loSD = richness - sd,
            hiSD = richness + sd) 
-  head(my.res)
+  bbreaks <- seq(min(my.res$year,na.rm=T),max(my.res$year,na.rm=T),1)
   
   plt <- my.res %>% 
     ggplot(aes(year,richness)) +
     geom_ribbon(aes(ymin=loSD,ymax=hiSD),alpha=0.6, fill = pal[4]) +
     geom_line(colour = pal[3],lwd=1.2) +
-    scale_x_continuous() +
+    scale_x_continuous(breaks = bbreaks) +
     ylab('Genre richness') +
     xlab('Year') +
     ggsidekick::theme_sleek(base_size=14) +
@@ -159,7 +165,6 @@ get_mds <- function(community,pal){
               inherit.aes = FALSE,size=5) +
     ggsidekick::theme_sleek(base_size=14)
   #ordplot
-  
   return(ordplot)
 }
 
