69d8a173ec97608bf11cc39e43527fa737fc85c7
Megsie Siple <mcsiple@gmail.com>
2020-03-27 18:15:37 -0700

    cleanup subjects

--- R/get_cmatrix_and_genres.R
+++ R/get_cmatrix_and_genres.R
@@ -1,26 +1,41 @@
 # get big fun matrix for further analysis
 get_cmatrix_and_genres <- function(fulldata){
-  #fulldata includes genres and everything, same exact format as scraped data (I think)
+  #fulldata includes genres and everything, same exact format as scraped data (I think )
   goodreads <- fulldata
   
   goodreads_read <- 
     goodreads %>%
     filter(exclusive_shelf =='read') %>%
     select(-book_genres)
-    #write_csv(path = here('output',
-    #                      'goodreads_read.csv'))
   
-  # save genre data
+  
+  # save genre data for read books
   genres <- goodreads %>%
+    filter(exclusive_shelf =='read') %>%
     select(book_genres,year_read) %>%
     mutate(id = 1:n()) %>%
-    unnest_longer(book_genres)
+    unnest_longer(book_genres) %>%
+    filter(!is.na(book_genres)) %>%
+    filter(!is.na(year_read)) %>%
+    filter(textcat::textcat(book_genres)=='english') %>%
+    mutate(book_genres = sub("\\ /.*", "", book_genres)) %>%
+    mutate(book_genres = sub("\\ --.*", "", book_genres)) %>%
+    mutate(book_genres = stringr::str_to_sentence(book_genres)) %>%
+    mutate(book_genres = trimws(book_genres))
   
   # save genres by month
   genres_month <- goodreads %>%
+    filter(exclusive_shelf =='read') %>%
     select(book_genres,month_read) %>%
     mutate(id = 1:n()) %>%
-    unnest_longer(book_genres)
+    unnest_longer(book_genres) %>%
+    filter(!is.na(book_genres)) %>%
+    filter(!is.na(month_read)) %>%
+    filter(textcat::textcat(book_genres)=='english') %>%
+    mutate(book_genres = sub("\\ /.*", "", book_genres)) %>%
+    mutate(book_genres = sub("\\ --.*", "", book_genres)) %>%
+    mutate(book_genres = stringr::str_to_sentence(book_genres)) %>%
+    mutate(book_genres = trimws(book_genres))
   
   # Turn genres by year into 'community matrix'
   community <-  genres %>% 
@@ -34,22 +49,23 @@ get_cmatrix_and_genres <- function(fulldata){
     as.data.frame()
   
   community_toread <- goodreads %>%
-    select(book_genres,month_read) %>%
+    select(book_genres,year_read) %>%
     mutate(id = 1:n()) %>%
     unnest_longer(book_genres) %>%
     pivot_longer(c(-id,-year_read)) %>% 
-    dplyr::count(year_read, value) %>% # to give each book its own diversity, change to dplyr::count(id, value)
+    dplyr::count(year_read, value) %>% 
     pivot_wider(
       names_from = value, 
       values_from = n, 
       values_fill = list(n = 0)
     ) %>%
     as.data.frame()
-    
-    
+  
+  
   return(list(community = community,
               community_toread = community_toread,
               genres = genres,
               genres_month = genres_month,
               goodreads_read = goodreads_read))
 }
+#get_cmatrix_and_genres(fulldata = nf)
